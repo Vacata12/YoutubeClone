794e4b7139d3fce136b5f58ccdc35c8026d416ff
Vacata12 <ivan.stoynev12@gmail.com>
2024-10-13 02:27:40 +0300

    Create login

--- website/__init__.py
+++ website/__init__.py
@@ -2,14 +2,18 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
+from werkzeug.utils import secure_filename
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
+UPLOAD_FOLDER = "/UploadFolder/"
+ALLOWED_EXTENSIONs = ["mkv", "MP4"]
 
 def createApp():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "YoutubeClone"
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
     db.init_app(app)
 
     from .views import views
--- website/auth.py
+++ website/auth.py
@@ -3,13 +3,36 @@ from flask_login import login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 import json
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods = ["GET", "POST"])
 def login():
-    return render_template("login.html")
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        user = User.query.filter_by(username=username).first()
+        
+        if user:
+            if not check_password_hash(user.password, password):
+                flash("Wrong password!", category="error")
+            else:
+                flash("Logged successfully!", category="success")
+                login_user = (user)
+                return redirect(url_for("views.home"))
+        else:
+            flash("User does not exist!", category="error")
+    else:
+        print("it's get")  
+    return render_template("login.html", user=current_user)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('views.home', user=current_user))
 
 @auth.route('/register', methods = ["GET", "POST"])
 def register():
@@ -34,7 +57,7 @@ def register():
             db.session.add(newUser)
             db.session.commit()
             flash("Account created", category="success")
-            return redirect(url_for("views.home"))
+            return redirect(url_for("views.home", user=current_user))
     elif request.method == "GET":
         print("Ivan Glupak")    
     return render_template("register.html")
