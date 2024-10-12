7979b5bb85729b9d4948351cabcd4e77768c9db0
Vacata12 <ivan.stoynev12@gmail.com>
2024-10-13 01:47:36 +0300

    make register

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template, request, flash, jsonify, redirect, url_for
 from flask_login import login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import User
 from . import db
 import json
 
@@ -9,6 +11,30 @@ auth = Blueprint('auth', __name__)
 def login():
     return render_template("login.html")
 
-@auth.route('/register')
+@auth.route('/register', methods = ["GET", "POST"])
 def register():
+    if request.method == "POST":
+        email = request.form.get("email")
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmPassword = request.form.get("confirmPassword")
+        
+        #check if there's already user with such email.
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash("Account with that email address already exist!", category="error")
+        elif len(email) < 4:
+            flash ("Email address is too short!", category="error")
+        elif password != confirmPassword:
+            flash ("Two passwords don't match!", category="error")
+        elif password == None or len(password) <= 7:
+            flash ("Password must be longer!", category="error")
+        else:
+            newUser = User(email=email, username = username, password = generate_password_hash(password))
+            db.session.add(newUser)
+            db.session.commit()
+            flash("Account created", category="success")
+            return redirect(url_for("views.home"))
+    elif request.method == "GET":
+        print("Ivan Glupak")    
     return render_template("register.html")
